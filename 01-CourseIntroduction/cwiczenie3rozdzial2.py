87183e438bab56578ab837b009e26793e6e7510e
Zuzanna Zimmer <114940033+ZuzannaZimmer@users.noreply.github.com>
2022-10-15 20:59:56 +0200

    zadanie

---
+++ 01-CourseIntroduction/cwiczenie3rozdzial2.py
@@ -0,0 +1,4 @@
+godziny = input("podaj liczbę godzin\n")
+stawka = input("podaj stawkę godzinową\n")
+wynagrodzenie = int(godziny) * float(stawka)
+print("wynagrodzenie", wynagrodzenie)
---
+++ 01-CourseIntroduction/cwiczenie5rozdzial2.py
@@ -0,0 +1,4 @@
+celsjusz = input("Podaj temperaturę w skali Celsjusza.\n")
+fahrenheit = float(celsjusz) + 30
+print("temperatura w skali Fahrenheita:")
+print(fahrenheit)
